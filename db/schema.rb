12931ce349be78e1a78df474a5b6deba0279fdc7
Matt <matthewprimpas@gmail.com>
2018-10-27 17:41:51 -0400

    Add Static Pages and style root landing page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_201658) do
+ActiveRecord::Schema.define(version: 2018_10_27_202017) do
 
   create_table "organizations", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2018_10_27_201658) do
     t.text "issues"
     t.string "gender"
     t.string "race"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "static_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
