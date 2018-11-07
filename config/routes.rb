ff17bb89c4a346d417435732d24bca3562187869
Matt <matthewprimpas@gmail.com>
2018-11-07 18:30:11 -0500

    Add Devise for user-org sessions and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :user_orgs
   resources :organizations
 
   root 'static_pages#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_202017) do
+ActiveRecord::Schema.define(version: 2018_11_07_232156) do
 
   create_table "organizations", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 2018_10_27_202017) do
   create_table "static_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_orgs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_user_orgs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_user_orgs_on_reset_password_token", unique: true
   end
 
 end
