2e49b759ac2062a5cbb5b23c45031c36d1277fa4
Matt <matthewprimpas@gmail.com>
2018-12-13 23:29:18 -0500

    Complete Organization table and create acceptable answer selections

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,14 @@ class Organization < ApplicationRecord
   belongs_to :user_org, :optional => true
   validates :name, presence: true
   validates :user_org, presence: true
+
+
+  def self.search(search_term)
+    if Rails.env != "production"
+      Organization.where(["name || issues || mission LIKE ?", "%#{search_term}%"])
+    else
+      Organization.where(["name || issues || mission iLIKE ?", "%#{search_term}%"])
+    end
+  end
+
 end
