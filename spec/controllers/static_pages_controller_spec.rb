2312d6541f474b7be9232cb6887cbb1ecedf9971
Matt Primpas <mattprimpas@bldup.com>
2019-07-29 15:24:21 -0400

    Remove unecessary quiz controller files

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe StaticPagesController, type: :controller do
 
   context 'GET #index' do
-    it 'renders the langing page layout template' do
+    it 'renders the landing page layout template' do
       get :index
       expect(response).to be_ok
       expect(response).to render_template(:index)
